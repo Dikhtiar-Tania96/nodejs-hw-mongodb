33b2708e0179723c3afae0cf045181bd8e667c91
Dikhtiar Tania <you@example.com>
2024-08-21 15:14:55 +0300

    new fix code

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -4,10 +4,11 @@ import {
   getContactsController,
   getContactByIdController,
 } from '../controllers/contacts.js';
+import {ctrlWrapper} from '../utils/ctrlWrapper.js';
 
 const router = express.Router();
 
-router.get('/', getContactsController);
-router.get('/:contactId', getContactByIdController);
+router.get('/', ctrlWrapper(getContactsController));
+router.get('/:contactId', ctrlWrapper(getContactByIdController));
 
 export default router;
