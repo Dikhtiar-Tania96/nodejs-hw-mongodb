4e714ce2615d4ea86442cf4c0a3983593f1b2e23
Dikhtiar Tania <you@example.com>
2024-08-22 18:42:23 +0300

    push new files

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -11,20 +11,49 @@ export const getAllContacts = async () => {
   };
 };
 
-
 export const getContactById = async(contactId) => {//повертає контакт за id
   const contact = await ContactCollection.findById(contactId);
   return contact;
 };
 
-export function createContact(payload){
-  return ContactCollection.create(payload);
-}
 
-export function deleteContact(contactId){
- return ContactCollection.findByIdAndDelete(contactId);
-}
 
+//створення нового контакту
+export const createContact = async (payload) => {
+  const contact = await ContactCollection.create(payload);
+  return contact;
+};
+
+
+//зміна даних у контакті
 export function patchContactEmail(contactId, email){
   return ContactCollection.findByIdAndUpdate(contactId, {onEmail: email}, {new:true});
 }
+// export const patchContactEmail = async (contactId, payload, options = {}) => {
+//   const opaResult = await ContactCollection.findOneAndUpdate(
+//     { _id: contactId },
+//     payload,
+//     {
+//       new: true,
+//       includeResultMetadata: true,
+//       ...options,
+//     },
+//   );
+//   if (!opaResult || !opaResult.value) return null;
+
+//   return {
+//     contact: opaResult.value,
+//     isNew: Boolean(opaResult?.lastErrorObject?.upserted),
+//   };
+// };
+
+//видалення контакту
+export function deleteContact(contactId){
+ return ContactCollection.findByIdAndDelete(contactId);
+}
+// export const deleteContact = async (contactId) => {
+//   const contact = await ContactCollection.findByIdAndDelete({
+//     _id: contactId,
+//   });
+//   return contact;
+// };
