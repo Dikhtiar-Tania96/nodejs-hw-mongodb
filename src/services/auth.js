27cdc44ed28f938d35b25be95411e43ae901a1df
Dikhtiar Tania <you@example.com>
2024-08-28 19:26:19 +0300

    push register

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
 import { UserCollection } from '../db/models/user.js';
 
@@ -7,6 +8,8 @@ export async function registerUser(payload) {
   if (maybeUser !== null) {
     throw createHttpError(409, 'Email in use');
   }
+
+  payload.password = await bcrypt.hash(payload.password, 10);
 
   return UserCollection.create(payload);
 
