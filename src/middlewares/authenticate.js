5c64489dcaf9c671539e130d5fdc33b6075b869b
Dikhtiar Tania <you@example.com>
2024-08-31 19:03:24 +0300

    push code

---
+++ src/middlewares/authenticate.js
@@ -0,0 +1,39 @@
+import createHttpError from 'http-errors';
+import { SessionCollection } from '../db/models/session.js';
+
+export async function authenticate(req, res, next) {
+  const { authenticate } = req.headers;
+
+  if (typeof authenticate !== 'string') {
+    return next(createHttpError(401, 'Please provide Authorization header'));
+  }
+  // const authHeader = req.get('Authorization');
+
+  // if (!authHeader) {
+  //   next(createHttpError(401, 'Please provide Authorization header'));
+  //   return;
+  // };
+
+  const [bearer, accessToken] = authenticate.split(' ', 2);
+  // const bearer = authHeader.split(' ')[0];
+  // const token = authHeader.split(' ')[1];
+
+  if (bearer !== 'Bearer' || typeof accessToken !== 'string') {
+    return next(createHttpError(401, 'Auth header should be of type Bearer'));
+  }
+
+  const session = await SessionCollection.findOne({ accessToken });
+
+  if (session === null) {
+    next(createHttpError(401, 'Session not found'));
+    return;
+  }
+
+  if (new Date() > new Date(session.accessTokenValidUntil)) {
+    return next(createHttpError(401, 'Access token expired'));
+  };
+
+  console.log(session);
+
+  next();
+}
