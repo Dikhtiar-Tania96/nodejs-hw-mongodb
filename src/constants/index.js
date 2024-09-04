7564017b650295c96b013e73e7e82c41aa3f2e9b
Dikhtiar Tania <you@example.com>
2024-09-04 16:05:54 +0300

    finish is 5branch

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,7 @@ export const SORT_ORDER = {
     DESC: 'desc',
   };
 
-  export const ACCESS_TOKEN_EXPIRY = 15 * 60 * 1000;//15hvs
+  export const ACCESS_TOKEN_EXPIRY = 15 * 60 * 1000 * 456789;//15hvs
   export const REFRESH_TOKEN_EXPIRY = 30 * 24 * 60 * 60 * 1000;
 
   export const SMTP = {
