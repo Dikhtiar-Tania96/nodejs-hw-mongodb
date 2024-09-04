f946e732b1f0ddcdc67021ef4a3cef0b8d179b26
Dikhtiar Tania <you@example.com>
2024-09-04 11:53:12 +0300

    oyps

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -41,7 +41,8 @@ export const getContactsController = async (req, res, next) => {
 export async function getContactByIdController(req, res, next) {
   const { contactId } = req.params;
   const userId = req.user._id;
-  const contact = await getContactById(contactId, userId);
+  const contact = await getContactById(contactId);
+  // const contact = await getContactById(contactId, userId);
   if (!contact) {
     throw createHttpError(404, 'Contact no found');  
   };
@@ -58,11 +59,13 @@ export async function getContactByIdController(req, res, next) {
 
 //створення контакту
 export async function createContactController(req, res) {
-  const userId = req.user._id;
-  const payload = {...req.body, userId};
-  const createdContact = await createContact(payload, userId);
-  console.log(createContact);
-  
+    const createdContact = await createContact(req.body);
+
+  // const userId = req.user._id;
+  // const payload = {...req.body, userId};
+  // const createdContact = await createContact(payload, userId);
+  // console.log(createContact);
+
   res.status(201).json({
     status: 201,
     message: 'Successfully created a contact!',
--- src/server.js
+++ src/server.js
@@ -6,9 +6,9 @@ import router from './routers/index.js';
 import { env } from './utils/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import contactsRoutes from './routers/contacts.js';
-import authContacts from './routers/auth.js';
-import {authenticate} from './middlewares/authenticate.js';
+// import contactsRoutes from './routers/contacts.js';
+// import authContacts from './routers/auth.js';
+// import {authenticate} from './middlewares/authenticate.js';
 
 const PORT = Number(env('PORT', '3000'));
 export const setupServer = () => {
