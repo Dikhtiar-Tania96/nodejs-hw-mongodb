2454b28f9f6a82c19e7016d3ee12b088516b487d
Dikhtiar Tania <you@example.com>
2024-09-04 15:33:42 +0300

    fix getallContacts

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -59,11 +59,11 @@ export async function getContactByIdController(req, res, next) {
 
 //створення контакту
 export async function createContactController(req, res) {
-  // const userId = req.user._id;
-  // const payload = {...req.body, userId};
-  // const createdContact = await createContact(payload, userId);
-  // console.log(createContact);
-    const createdContact = await createContact(req.body);
+  const userId = req.user._id;
+  const payload = {...req.body, userId};
+  const createdContact = await createContact(payload, userId);
+  console.log(createContact);
+    // const createdContact = await createContact(req.body);
   res.status(201).json({
     status: 201,
     message: 'Successfully created a contact!',
